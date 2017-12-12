8a716a8d10854d953502a394247c03223da8ed99
Cesar Augusto Gomez Bustamante <cgomezb@pslcol.com.co>
2017-12-11 20:41:46 -0500

    Create a form, textbox, checkbox, radio and select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  languages = ['English', 'Spanish', 'Other'];
+  model = new Employee('Cesar', 'Gomez', true, 'payOne', 'English');
 }
---
+++ src/models/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public isFullTime: boolean,
+        public paymentType: string,
+        public primaryLanguage: string
+    ) {
+
+    }
+}
